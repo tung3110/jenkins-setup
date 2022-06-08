47e261032aeaa89f45fd61e341b761f8c8e5447d
tung3110 <nguyenhuytung@gmail.com>
2022-06-08 17:01:22 +0700

    first commit

--- apijenkins.sh
+++ apijenkins.sh
@@ -2,4 +2,4 @@
 #!/bin/sh
 #docker rmi  tungnh2022/apihello:latest
 #docker container stop myjenkins
-docker run -d -p 3000:3000 --name myjenkins -rm tungnh2022/apihello:latest
+docker run -d -p 3000:3000 --name myjenkins --rm tungnh2022/apihello:latest
