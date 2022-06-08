6c73ea151959016efda88573c79f086fd16c064b
tung3110 <nguyenhuytung@gmail.com>
2022-06-08 14:36:54 +0700

    first commit

--- apijenkins.sh
+++ apijenkins.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+docker image prune
 docker container stop $(docker container ls -q --filter ancestor=tungnh2022/jenkins-setup:latest)
 docker pull tungnh2022/jenkins-setup:latest
 docker run -d -p 3000:3000 tungnh2022/jenkins-setup:latest
