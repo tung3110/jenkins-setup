d6c6ca6fc8c2c043f266bb46a9efa218d3cfe02c
tung3110 <nguyenhuytung@gmail.com>
2022-06-08 16:49:59 +0700

    first commit

--- apijenkins.sh
+++ apijenkins.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-docker rm $(docker stop $(docker ps -a -q -f ancestor=<tungnh2022/jenkins-setup:latest>))
-docker pull tungnh2022/jenkins-setup:latest
-docker run -d -p 3000:3000 tungnh2022/jenkins-setup:latest
+#!/bin/sh
+#docker rmi  tungnh2022/apihello:latest
+docker container stop myjenkins
+docker run -d -p 3000:3000 --name myjenkins -rm tungnh2022/apihello:latest
